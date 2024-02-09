54de03787290e7879ebac676fe02f21d01cc9df5
Collince Owinoh <coowinoh@gmail.com>
2024-02-09 15:31:09 +0300

    ft-all pages update, ft-javascript

--- js/main.js
+++ js/main.js
@@ -96,3 +96,67 @@
     
 })(jQuery);
 
+
+let services = [
+    {name:"Solar Panels", img: "", description:"Moriah Solar and Electricals stands at the forefront of solar innovation, offering comprehensive installation services designed to power homes and businesses with clean, renewable energy. With a steadfast commitment to sustainability and excellence, we deliver top-tier solutions tailored to meet the diverse needs of our clientele. Our team of seasoned professionals brings a wealth of expertise to every project, ensuring seamless integration and optimal performance of solar systems. From residential rooftops to sprawling commercial complexes, we specialize in the design, installation, and maintenance of cutting-edge solar arrays that harness the power of the sun to reduce carbon footprints and lower energy costs. At Moriah Solar and Electricals, we're not just installing solar panels – we're empowering communities to embrace a greener, more sustainable future."},
+    {name:"CCTV Solutions", img: "", description:"Step into a world of enhanced security and peace of mind with Moriah Solar and Electricals' comprehensive CCTV installation services. As a trusted leader in the field of surveillance technology, we specialize in providing cutting-edge CCTV solutions tailored to meet the diverse security needs of residential, commercial, and industrial clients. With a focus on innovation and reliability, our skilled technicians deploy state-of-the-art CCTV systems designed to deter intruders, monitor activities, and safeguard your property round the clock. From strategic camera placement to seamless integration with existing security infrastructure, we offer end-to-end solutions that prioritize safety and security above all else."},
+    {name:"Electric Fencing", img: "", description:"Introducing Moriah Solar and Electricals, your premier destination for cutting-edge electric fencing solutions. At Moriah Solar and Electricals, we pride ourselves on being the forefront of innovation in the realm of security and protection. With a keen focus on quality craftsmanship and state-of-the-art technology, we deliver bespoke electric fencing installations tailored to safeguard your property with unparalleled efficiency and reliability. Our team of skilled professionals combines years of industry expertise with a commitment to excellence, ensuring that every project is executed with precision and care. Whether you're looking to fortify your residential estate or secure your commercial premises, trust Moriah Solar and Electricals to provide comprehensive electric fencing services that exceed expectations. With us, your safety is our top priority, and we're dedicated to delivering peace of mind through superior security solutions."},
+    {name:"Wiring",  img: "", description:"Welcome to Moriah Solar and Electricals, where expertise meets efficiency in the realm of electrical wiring solutions. With a steadfast commitment to safety and reliability, we specialize in providing top-tier wiring services tailored to meet the unique needs of residential and commercial properties alike. Our team of skilled technicians brings years of experience to every project, ensuring precision and quality in every installation. From new construction projects to renovation endeavors, we offer comprehensive wiring solutions that adhere to the highest industry standards and regulations. At Moriah Solar and Electricals, we understand the importance of a robust electrical infrastructure in powering modern living and business operations."},
+]
+const serviceItems = document.querySelectorAll(".service-items");
+// serviceItems.style.background = "red";
+const popup = document.querySelector(".popup-box")
+const popupCloseBtn = popup.querySelector(".popup-close-btn");
+const popupCloseIcon = popup.querySelector(".popup-close-icon");
+const popupTitle = popup.querySelector("h1");
+const popupImage = popup.querySelector("img");
+const popupDescription = popup.querySelector("p");
+console.log("services", serviceItems.length);
+
+serviceItems.forEach(service => {
+
+    service.addEventListener("click", function(event) {
+    
+        popupBox(event);
+    
+    
+    
+    })
+    
+})
+popupCloseBtn.addEventListener("click", function(event) {
+   
+    popupBox(event);
+
+
+
+})
+
+
+popupCloseIcon.addEventListener("click", function(event) {
+   
+    popupBox(event);
+
+
+
+})
+
+
+function popupBox(event) {
+
+    console.log("target", event.target.closest(".service-items"))
+    if (event.target.closest(".service-items")) {
+
+        // setup dialog box
+
+        let serviceDetails = services[[...serviceItems].indexOf(event.target.closest(".service-items"))];
+        console.log("serviceDetails", serviceDetails)
+        popupTitle.textContent = serviceDetails.name
+        
+        popupDescription.textContent = serviceDetails.description
+    }
+    popup.classList.toggle("open");
+
+   
+
+}
